6a7d264990830a6ac725abb976b426a87592e15b
Bscl <brayanstidcorteslombana@gmail.com>
2024-09-26 15:56:06 -0500

    inicio de las funcionalidades del inicio de sesion y registro

---
+++ js/campos_completos.js
@@ -0,0 +1,7 @@
+// function campos (){
+//     if (campos){
+//         return "Campos obrigatórios";
+//     }else{
+//         return "";
+//     }
+// } inicio de la funcionalidad haciendo un comit
