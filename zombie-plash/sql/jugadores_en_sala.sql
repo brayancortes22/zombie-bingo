e3cbc30af07b33bfc8da1bcb41319e315184338c
bscl <brayanstidcorteslombana@gmai.com>
2024-11-21 16:47:22 -0500

    una parte del juego en desarrollo

---
+++ zombie-plash/sql/jugadores_en_sala.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-11-2024 a las 02:43:48
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `zombie_plash_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugadores_en_sala`
+--
+
+CREATE TABLE `jugadores_en_sala` (
+  `id` int(11) NOT NULL,
+  `id_sala` int(11) DEFAULT NULL,
+  `id_jugador` int(11) DEFAULT NULL,
+  `nombre_jugador` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `jugadores_en_sala`
+--
+
+INSERT INTO `jugadores_en_sala` (`id`, `id_sala`, `id_jugador`, `nombre_jugador`) VALUES
+(162, 115, 1, 'bscl'),
+(163, 116, 1, 'bscl'),
+(164, 117, 1, 'bscl'),
+(166, 119, 1, 'bscl'),
+(173, 123, 1, 'bscl'),
+(174, 123, 10, 'jhon');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `jugadores_en_sala`
+--
+ALTER TABLE `jugadores_en_sala`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_sala` (`id_sala`),
+  ADD KEY `id_jugador` (`id_jugador`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `jugadores_en_sala`
+--
+ALTER TABLE `jugadores_en_sala`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=175;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `jugadores_en_sala`
+--
+ALTER TABLE `jugadores_en_sala`
+  ADD CONSTRAINT `jugadores_en_sala_ibfk_1` FOREIGN KEY (`id_sala`) REFERENCES `salas` (`id_sala`),
+  ADD CONSTRAINT `jugadores_en_sala_ibfk_2` FOREIGN KEY (`id_jugador`) REFERENCES `jugador` (`id_jugador`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
